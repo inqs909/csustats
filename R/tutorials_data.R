b5f0a76e25bcba29e686368f07c2303362f52a93
Isaac Quintanilla Salinas <inqs@pop-os.localdomain>
2023-06-01 16:09:58 -0700

    Updates

--- R/tutorials_data.R
+++ R/tutorials_data.R
@@ -1,10 +1,27 @@
-ID <- c("R_SLR", "R_MLR", "R_Resid")
+ID <- c("Intro_R", "R_Basics", "R_Graphics",
+        "R_Stats", "R_Hyp",
+        "R_ANOVA", "R_design",
+        "R_SLR", "R_MLR", "R_Resid",
+        "R_Tidy", "R_ggplot2")
 
-Subject <- c("Linear Regression", "Linear Regression", "Linear Regression")
+Subject <- c(rep("R Basics", 3),
+             rep("Basic Statistics", 2),
+             rep("ANOVA", 2),
+             rep("Linear Regression", 3),
+             rep("Tidyverse", 2))
 
-Description <- c("This tutorial details how to conduct a simple linear regression model in R.",
+Description <- c("This tutorial is an introduction to R.",
+                 "This tutorial details on R Basics",
+                 "This tutorial details on graphics",
+                 "This tutorial details how to conduct a basic statistics in R.",
+                 "This tutorial details how to conduct hypothesis testing in R.",
+                 "This tutorial details how to conduct an ANOVA in R.",
+                 "This tutorial details how to conduct an various ANOVA methods in R.",
+                 "This tutorial details how to conduct a simple linear regression model in R.",
                  "This tutorial details how to conduct a multiple linear regression model in R.",
-                 "This tutorial details how to conduct a residual analysis in R.")
+                 "This tutorial details how to conduct a residual analysis in R.",
+                 "This tutorial details how to use tidyverse packages",
+                 "This tutorial details how to create plots using ggplot2.")
 
 tutorials <- data.frame(ID=ID,
                         Subject = Subject,
